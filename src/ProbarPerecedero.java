ffde83935d5357b7bc936ad97a07dc59eb7751dc
JoaoCzz <joaoczz0205@gmail.com>
2023-11-06 19:04:17 +0100

    :l

--- src/ProbarPerecedero.java
+++ src/ProbarPerecedero.java
@@ -1,5 +1,13 @@
-public class Main {
+import com.corenetworks.Modelo.Producto;
+import com.corenetworks.Modelo.ProductoPerecedero;
+
+public class ProbarPerecedero {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ProductoPerecedero p1= new ProductoPerecedero("Leche",2,1.2,"2023/12/31");
+        ProductoPerecedero p2= new ProductoPerecedero("Leche",2,1.2,"2023/12/31");
+        System.out.println(p1.toString());
+        Producto P1= new Producto("Pan",1,0.8);
+        System.out.println(P1.toString());
+        System.out.println(p1.equals(p2));
     }
 }
--- src/com/corenetworks/Modelo/ProductoPerecedero.java
+++ src/com/corenetworks/Modelo/ProductoPerecedero.java
@@ -1,2 +1,33 @@
-package com.corenetworks.Modelo;public class ProductoPerecedero {
+package com.corenetworks.Modelo;
+
+public class ProductoPerecedero extends Producto{
+private String caducidad;
+
+    @Override
+    public String toString() {
+        return "ProductoPerecedero{" +
+                "caducidad='" + caducidad + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", cantidad=" + cantidad +
+                ", pvp=" + pvp +
+                '}';
+    }
+
+
+    public ProductoPerecedero(String caducidad) {
+        this.caducidad = caducidad;
+    }
+
+    public ProductoPerecedero(String nombre, int cantidad, double pvp, String caducidad) {
+        super(nombre, cantidad, pvp);
+        this.caducidad = caducidad;
+    }
+
+    public String getCaducidad() {
+        return caducidad;
+    }
+
+    public void setCaducidad(String caducidad) {
+        this.caducidad = caducidad;
+    }
 }
